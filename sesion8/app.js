7e1d9c51dd4a4bb80964fe0f0122120a01c16684
Dionicio <dioniciomatamacias@gmail.com>
2022-10-04 19:41:05 -0500

    sesion 8 started

---
+++ sesion8/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./config/db');
+const routes = require('./routes/index');
+const auth = require('./config/auth')
+
+const app = express();
+app.use(express.json());
+app.use(auth.opcional);
+app.use('/', routes);
+
+try {
+    sequelize.authenticate();
+    sequelize.sync();
+    console.log("Connected to DB");
+} catch (error){
+    console.log("unable to connect to DB: ", error);
+}
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log("Server listening on PORT 3000");
+});
